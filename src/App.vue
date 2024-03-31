b6292d393ad03e8a815980f6b3e3ab25f19b1905
Tane4ka170 <tane4ka170@gmail.com>
2024-04-01 00:20:43 +0300

    lesson 3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
-import HomepageView from './views/HomepageView.vue'
+// import HomepageView from './views/HomepageView.vue'
+import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
 </script>
 
 <template>
-  <HomepageView />
+  <!-- <HomepageView /> -->
+  <div class="bg-white h-screen w-[400px]">
+    <FavoritePlaces>
+      <div>THis is super slot</div>
+      <template v-slot:label>This is label</template>
+      <template #list>This is list</template>
+    </FavoritePlaces>
+  </div>
 </template>
