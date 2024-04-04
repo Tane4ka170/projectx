01b694e20696f51aa52bda6789a4d28be7d35728
Tane4ka170 <tane4ka170@gmail.com>
2024-04-04 23:46:46 +0300

    computed

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -3,22 +3,19 @@ import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
 import { ref } from 'vue'
 
-const counter = ref(0)
-const user = ref({ name: 'Tom', age: 14 })
-
-const increment = () => {
-  counter.value += 1
+const buttonVariant = ref('gradient')
+const changeButtonVariant = () => {
+  buttonVariant.value = buttonVariant.value === 'gradient' ? 'outlined' : 'gradient'
 }
 </script>
 
 <template>
   <div class="px-6">
-    <div class="text-gray">User {{ user.name }}</div>
-    <div class="text-grey mb-4">Додані маркери({{ counter }})</div>
+    <div class="text-grey mb-4">Додані маркери({{ buttonVariant }})</div>
     <slot name="label"></slot>
     <slot name="list">
       <FavoritePlace v-for="n in 4" :key="n" />
     </slot>
-    <IButton class="w-full mt-10" variant="gradient1" @click="increment">Додати маркер</IButton>
+    <IButton class="w-full mt-10" :variant="gradient1" @click="increment">Додати маркер</IButton>
   </div>
 </template>
--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, computed } from 'vue'
 
 const props = defineProps({
   variant: {
@@ -10,7 +10,7 @@ const props = defineProps({
     }
   }
 })
-const bgStyles =
+const bgStyles = computed =(()={})
   props.variant === 'gradient' ? 'bg-gradient-to-r from-[#FFA279] to-[#F3743D]' : 'bg-[#FFA279]'
 </script>
 
