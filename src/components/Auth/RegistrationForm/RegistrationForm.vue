4b8d0037018130dd4d070ebb5b8a7e57c8da3a91
Tane4ka170 <tane4ka170@gmail.com>
2024-04-08 00:36:16 +0300

    v-model, started watch

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -1,18 +1,25 @@
 <script setup>
+import { reactive, toRaw } from 'vue'
 import IButton from '@/components/IButton/IButton.vue'
 import IInput from '@/components/IInput/IInput.vue'
+import FormContainer from '../FormContainer.vue'
 
 const emit = defineEmits(['submit'])
+const userData = reactive({
+  name: '',
+  email: '',
+  password: ''
+})
 </script>
 
 <template>
-  <form
+  <FormContainer
     class="max-w-[500px] w-full bg-white p-10 rounded-2xl"
-    @submit.prevent="emit('submit', { name: 'NAme' })"
+    @submit.prevent="emit('submit', toRaw(userData))"
   >
-    <IInput label="Повне ім'я" class="mb-4" />
-    <IInput label="Електронна пошта" placeholder="email@gmail.com" />
-    <IInput label="Пароль" type="password" disabled />
+    <IInput label="Повне ім'я" class="mb-4" v-model="userData.name" />
+    <IInput label="Електронна пошта" placeholder="email@gmail.com" v-model="userData.email" />
+    <IInput label="Пароль" type="password" disabled v-model="userData.password" />
     <IButton class="mt-5" variant="gradient" type="submit">Створити аккаунт</IButton>
-  </form>
+  </FormContainer>
 </template>
--- src/components/IInput/IInput.vue
+++ src/components/IInput/IInput.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
 const props = defineProps({
+  modelValue: String,
   label: String,
   placeholder: String,
   type: {
@@ -14,10 +15,12 @@ defineOptions({
   inheritAttrs: false
 })
 
-const text = ref('')
-const changeText = (event) => {
-  text.value = event.target.value
-}
+// const text = ref('')
+// const changeText = (event) => {
+//   text.value = event.target.value
+// }
+
+const emit = defineEmits(['update:modelValue'])
 </script>
 
 <template>
@@ -27,7 +30,8 @@ const changeText = (event) => {
       <input
         class="w-full text-sm rounded-[4px] border-[1px] py-2 px-3 border-[#eaeaea] focus:outline-primary"
         v-bind="{ ...$props, ...$attrs }"
-        @input="changeText"
+        :value="modelValue"
+        @input="emit('update:modelValue', $event.target.value)"
       />
     </label>
   </div>
