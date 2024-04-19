8baf4f09cd4c1be75c0f149c00a26c6f657c4d59
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-04-19 20:48:42 +0300

    Base modal. <Teleport>

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup>
 // import HomepageView from './views/HomepageView.vue'
 // import FavoritePlaces from './components/FavoritePlaces/FavoritePlaces.vue'
+import { ref } from 'vue'
 import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
 import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
+import IModal from './components/IModal/IModal.vue'
+const isOpen = ref(false)
+const closeModal = () => {
+  isOpen.value = false
+}
+const openModal = () => {
+  isOpen.value = true
+}
 </script>
 
 <template>
@@ -10,6 +19,8 @@ import RegistrationForm from './components/Auth/RegistrationForm/RegistrationFor
   <!-- <div class="bg-white h-screen w-[400px]">
     <FavoritePlaces />
   </div> -->
+  <button @click="openModal">Click</button>
   <RegistrationForm @submit="console.log" />
   <LoginForm />
+  <IModal @close="closeModal" v-if="isOpen">Zorra</IModal>
 </template>
